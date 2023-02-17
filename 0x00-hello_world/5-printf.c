205f08253b8fe2030933da832b5920a907b7c393
BereketeAb <BereketeAbbirkuk@gmail.com>
2023-02-16 18:57:26 -0800

    deadline

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ * main -First C prog
+ *
+ * Description: puts,
+ */
+
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+  printf("with proper grammar, but the outcome is a piece of art,");
+  return (0);
+}
